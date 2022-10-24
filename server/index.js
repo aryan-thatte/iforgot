67b606f3e33210ab64673d64b15ce07e7be4d130
Aryan Thatte <aryan.thatte@gmail.com>
2022-10-24 00:14:09 -0400

    be progress and default html removal

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+import bodyParser = require("body-parser")
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    dbName: 'mainDB'
+})
 
 app.get("/", (req, res) => {
     res.send("hello world")
 })
 
 app.listen(3001, () => {
-    
+
 }) // port for server
 
